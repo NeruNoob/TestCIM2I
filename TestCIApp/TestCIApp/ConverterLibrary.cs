b155de58c31a35943fc44ac8775161dedf9cd528
page_e <elie.page@epitech.eu>
2020-05-12 16:15:31 +0200

    Ajout GetInt dans la lib

---
+++ TestCIApp/TestCIApp/ConverterLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCIApp
+{
+    public class ConverterLibrary
+    {
+        public int GetInt(string nbr)
+        {
+            return int.Parse(nbr) + 1; // Ook !
+        }
+    }
+}

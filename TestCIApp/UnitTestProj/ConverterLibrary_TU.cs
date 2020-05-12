78f3e3c543bb036b53cf1646bf0fad566ce0d2cf
page_e <elie.page@epitech.eu>
2020-05-12 16:31:45 +0200

    Ajout ConverterLibrary_TU GetIntTest

---
+++ TestCIApp/UnitTestProj/ConverterLibrary_TU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCIApp;
+
+namespace UnitTestProj
+{
+    [TestClass]
+    public class ConverterLibrary_TU
+    {
+        [TestMethod]
+        [DataRow("1", 1)]
+        [DataRow("13", 13)]
+        [DataRow("-3", -3)]
+        public void TestGetInt(string target, int expected)
+        {
+            //Arrange
+            ConverterLibrary lib = new ConverterLibrary();
+
+            //Act
+            var tmp = lib.GetInt(target);
+
+            //Assert
+            Assert.AreEqual(expected, tmp);
+        }
+    }
+}
---
+++ TestCIApp/UnitTestProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProj")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8bff95b-f507-4e75-9125-e0cc31a7994a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
